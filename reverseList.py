577ee2b7995263c777134fa68e0a3f05b268c48b
kjolern <tom.kjolhamar@gmail.com>
2022-10-06 22:20:34 +0000

    add reverseList program

---
+++ reverseList.py
@@ -0,0 +1,9 @@
+#Python program to reverse a list
+
+numberslist=[1,2,3,4,5,6,7,8,9,10]
+
+print("The numberlist is originally {}".format(numberslist))
+
+numberslist.reverse()
+
+print("The numberlist reversed is {}".format(numberslist))
