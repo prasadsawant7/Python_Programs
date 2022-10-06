b9c725e85583ed6be30d2c23de42179662f63d8b
Rafael Dias <rafael.cdc97@gmail.com>
2022-10-06 18:59:53 -0300

    feat/issue-39 - add comment

--- positiveListValue.py
+++ positiveListValue.py
@@ -1,6 +1,10 @@
-list1 = [0, -10, 21, 4, -45, -66, 93, -11]
- 
-for num in list1:
+# Python program to print positive numbers in a list
+# List of numbers
+listNumbers = [0, -10, 21, 4, -45, -66, 93, -11]
+
+# Iterating each number in list
+for num in listNumbers:
+
     # checking condition
     if num >= 0:
         print(num)
