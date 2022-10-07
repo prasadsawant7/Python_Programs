fbaadc109fe7a344c2df506c6f2ccace21086522
snehal-s10 <114774728+snehal-s10@users.noreply.github.com>
2022-10-08 01:30:33 +0530

    Add armstrong.py

---
+++ armstrong.py
@@ -0,0 +1,12 @@
+n = 153
+s = n
+b = len(str(n))
+sum1 = 0
+while n != 0:
+    r = n % 10
+    sum1 = sum1+(r**b)
+    n = n//10
+if s == sum1:
+    print("The given number", s, "is armstrong number")
+else:
+    print("The given number", s, "is not armstrong number")
